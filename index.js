8a3ab5d4eaad2f4ea4d4b971e5c52dd2266ff60b
kakahan01 <34622630+kakahan01@users.noreply.github.com>
2019-01-22 23:02:38 +0300

    test

---
+++ index.js
@@ -0,0 +1,28 @@
+var stringDifference = function() {
+  this.calculateDifference = function(string1, string2) {
+    if (typeof(string1) !== "string" || typeof(string2) !== "string") {
+      throw "CalculateError: String1 or String2 should be a string.";
+    }
+
+    if (string1.length !== string2.length) {
+      throw "CalculateError: String1.length and String2.length should be same.";
+    }
+
+    var s1 = string1.split("");
+    var s2 = string2.split("");
+
+    var output = [];
+
+    for (var i = 0; i < s1.length; i++) {
+      var char = s1[i];
+      var char2 = s2[i];
+
+      if (char == char2) output.push(true);
+      else output.push(false);
+    }
+
+    return output;
+  }
+};
+
+module.exports = stringDifference;
